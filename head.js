9187e69555338fffa18ed409cf743a4798e4e9ae
edart2 <65454559+edart2@users.noreply.github.com>
2024-03-07 23:20:02 -0500

    Update head.js

--- head.js
+++ head.js
@@ -1,17 +1,1 @@
-<script async src="https://se.wized.com/bJ3YENnavzt2etBGoios.js"></script>
-<script async type="module" data-wized-id="bJ3YENnavzt2etBGoios" src="https://se.wized.com/v2/index.js"></script>
 
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Resizable - Default functionality</title>
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  <style>
-  #resizable { width: 150px; height: 150px; padding: 0.5em; }
-  #resizable h3 { text-align: center; margin: 0; }
-  </style>
-  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-
- 
