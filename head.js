d0165ec14a6271577081a4d0e2103f5de68e3924
edart2 <65454559+edart2@users.noreply.github.com>
2024-03-07 23:11:06 -0500

    Update head.js

--- head.js
+++ head.js
@@ -13,6 +13,7 @@
   </style>
   <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
   <script>
   $( function() {
     $( "#resizable" ).resizable();
